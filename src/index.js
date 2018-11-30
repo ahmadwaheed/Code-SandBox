9e2d5fa87b9742702e861f6bdbf84e06d50cc62a
Ahmad Waheed <ammwaheed@ucdavis.edu>
2018-11-29 17:20:19 -0800

    adding greeting
    greeting

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>
-        Start editing to see some magic happen! This is pretty cool, I love it{" "}
+        Start editing to see some magic happen! This is pretty cool, I love the
+        architecture and the way we can integrate it with GitHub and Editor.
       </h2>
     </div>
   );
