eb97427837ca4076a741ad34b320035d73624041
Ahmad Waheed <ammwaheed@ucdavis.edu>
2018-11-29 17:21:52 -0800

    sync
    sync

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>
         Start editing to see some magic happen! This is pretty cool, I love the
-        architecture and the way we can integrate it with GitHub and Editor.
+        architecture and the way we can integrate it with GitHub and Editor. I
+        like this sync idea.
       </h2>
     </div>
   );
